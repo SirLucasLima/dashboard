8e69283425eae378f33e4dd72ac6cca403792fc9
Lucas Lima <developerlucaslima@gmail.com>
2023-03-21 13:39:12 -0300

    hire me icon animation

--- src/components/banner/styles.jsx
+++ src/components/banner/styles.jsx
@@ -21,9 +21,15 @@ export const Container = styled.div`
   }
 
   button {
-    width: 13.8rem;
+    width: auto;
+    padding: 0 2.4rem;
     height: 8rem;
     border-radius: 24px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .8rem;
 
     font-weight: 700;
     font-size: 2.4rem;
@@ -31,5 +37,41 @@ export const Container = styled.div`
 
     color: ${({theme}) => theme.textColor};
     background-color: ${({theme}) => theme.purple};
+
+    i {
+      font-size: 2.4rem;
+      color: ${({theme}) => theme.rose};
+
+      -moz-animation: pulse 2s infinite linear;
+      -o-animation: pulse 2s infinite linear;
+      -webkit-animation: pulse 2s infinite linear;
+      animation: pulse 2s infinite linear;
+    }
+
+    @-webkit-keyframes pulse {
+      0% { opacity: 1; color: ${({theme}) => theme.yellow}}
+      50% { opacity: 0; color: ${({theme}) => theme.rose}}
+      100% { opacity: 1; color: ${({theme}) => theme.yellow}}
+    }
+    @-moz-keyframes pulse {
+      0% { opacity: 1; color: ${({theme}) => theme.yellow}}
+      50% { opacity: 0; color: ${({theme}) => theme.rose}}
+      100% { opacity: 1; color: ${({theme}) => theme.yellow}}
+    }
+    @-o-keyframes pulse {
+      0% { opacity: 1; color: ${({theme}) => theme.yellow}}
+      50% { opacity: 0; color: ${({theme}) => theme.rose}}
+      100% { opacity: 1; color: ${({theme}) => theme.yellow}}
+    }
+    @-ms-keyframes pulse {
+      0% { opacity: 1; color: ${({theme}) => theme.yellow}}
+      50% { opacity: 0; color: ${({theme}) => theme.rose}}
+      100% { opacity: 1; color: ${({theme}) => theme.yellow}}
+    }
+    @keyframes pulse {
+      0% { opacity: 1; color: ${({theme}) => theme.yellow}}
+      50% { opacity: 0; color: ${({theme}) => theme.rose}}
+      100% { opacity: 1; color: ${({theme}) => theme.yellow}}
+    }
   }
 `
