f552735b55c8676c2da013e3f753c6866953c280
Lucas Lima <developerlucaslima@gmail.com>
2023-04-14 09:26:57 -0300

    metaversus data done

--- src/data/DevProjects.js
+++ src/data/DevProjects.js
@@ -1,8 +1,43 @@
-import display from "../assets/display.png"
+import airBnbClone1 from "../assets/projectsDev/airBnbClone1.png"
+import airBnbClone2 from "../assets/projectsDev/airBnbClone2.png"
+import metaversus1 from "../assets/projectsDev/metaversus1.png"
+import metaversus2 from "../assets/projectsDev/metaversus2.png"
+import notes1 from "../assets/projectsDev/notes1.png"
+import notes2 from "../assets/projectsDev/notes2.png"
+import portofolio1 from "../assets/projectsDev/portofolio1.png"
+import portofolio2 from "../assets/projectsDev/portofolio2.png"
+import socialMedia1 from "../assets/projectsDev/socialMedia1.png"
+import socialMedia2 from "../assets/projectsDev/socialMedia2.png"
 
 export const DevProjects = [
   {
-    "img": display,
+    "img1": metaversus1,
+    "img2": metaversus2,
+    "title": "Metaversus - Front-End Project",
+    "links": {
+      "deploy": "https://metaversus-client.vercel.app/", 
+      "frontend": "https://github.dev/developerlucaslima/metaversusCLIENT",
+    },
+    "description": "Este é um projeto moderno com belas animações, estilizações e uma paleta de cores atraente. Foi uma das minhas primeiras experiências com as tecnologias Next.js, Tailwind.css e Framer Motion. Fiquei impressionado com o Next.js por oferecer suporte a server rendering e pretendo utilizá-lo em meus projetos futuros. Ao combinar essas tecnologias, pude aproveitar a facilidade de desenvolvimento do Tailwind e a capacidade do Framer Motion para adicionar animações personalizadas e transições suaves em componentes React. Além disso, Tailwind é altamente configurável, permitindo a personalização das classes de estilo para atender às necessidades específicas de cada projeto.",
+    "date": "Janeiro de 2023",
+    "techs": "Next.js, Tailwind.Css, Framer Motion"
+  },
+  {
+    "img1": notes1,
+    "img2": notes2,
+    "title": "Notes - Full-Stack Project",
+    "links": {
+      "deploy": "https://metaversus-client.vercel.app/",
+      "frontend": "https://github.com/developerlucaslima/notesCLIENT",
+      "backend": "https://github.com/developerlucaslima/notesAPI",
+    },
+    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis consectetur labore, distinctio unde pariatur ratione! Totam distinctio corporis ipsam beatae quam rerum obcaecati quidem provident commodi quibusdam, maiores nostrum magnam ratione velit. Repudiandae doloremque fugit ut fuga distinctio, facilis voluptates earum deserunt ad ullam, vero esse necessitatibus velit et cum assumenda quasi id quidem? Ea obcaecati dolores cum aliquam, saepe nostrum quo! Iusto placeat nam reprehenderit rem consectetur. In repellat a atque quos explicabo iure incidunt quibusdam et possimus fuga. Ex vel assumenda ratione, ullam ea doloremque maxime! Animi neque adipisci aliquid, veniam rem exercitationem quisquam quas? Maxime laudantium obcaecati ex quidem nemo. Iusto incidunt delectus necessitatibus qui possimus explicabo, reiciendis voluptatibus magnam, facilis itaque corrupti debitis, tempore distinctio dolorem soluta iure aspernatur culpa nihil? Maxime fugit deleniti quis quasi. Cum, nulla corporis quam tempora impedit alias fuga optio possimus voluptates, deleniti eum assumenda sint est mollitia ipsa ducimus cumque!",
+    "date": "Outubro de 2022",
+    "techs": "JavaScript, Node.JS"
+  },
+  {
+    "img1": socialMedia1,
+    "img2": socialMedia2,
     "title": "Projetinho Dev",
     "links": {
       "deploy": "https://web.whatsapp.com", 
@@ -14,7 +49,8 @@ export const DevProjects = [
     "techs": "JavaScript, Node.JS"
   },
   {
-    "img": display,
+    "img1": portofolio1,
+    "img2": portofolio2,
     "title": "Projetinho Dev",
     "links": {
       "deploy": "https://web.whatsapp.com", 
@@ -26,31 +62,8 @@ export const DevProjects = [
     "techs": "JavaScript, Node.JS"
   },
   {
-    "img": display,
-    "title": "Projetinho Dev",
-    "links": {
-      "deploy": "https://web.whatsapp.com", 
-      "frontend": "https://app.rocketseat.com.br/dashboard", 
-      "backend": "https://github.com/sirlucaslima", 
-      "behance": "https://www.behance.net/"
-    },
-    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis consectetur labore, distinctio unde pariatur ratione! Totam distinctio corporis ipsam beatae quam rerum obcaecati quidem provident commodi quibusdam, maiores nostrum magnam ratione velit. Repudiandae doloremque fugit ut fuga distinctio, facilis voluptates earum deserunt ad ullam, vero esse necessitatibus velit et cum assumenda quasi id quidem? Ea obcaecati dolores cum aliquam, saepe nostrum quo! Iusto placeat nam reprehenderit rem consectetur. In repellat a atque quos explicabo iure incidunt quibusdam et possimus fuga. Ex vel assumenda ratione, ullam ea doloremque maxime! Animi neque adipisci aliquid, veniam rem exercitationem quisquam quas? Maxime laudantium obcaecati ex quidem nemo. Iusto incidunt delectus necessitatibus qui possimus explicabo, reiciendis voluptatibus magnam, facilis itaque corrupti debitis, tempore distinctio dolorem soluta iure aspernatur culpa nihil? Maxime fugit deleniti quis quasi. Cum, nulla corporis quam tempora impedit alias fuga optio possimus voluptates, deleniti eum assumenda sint est mollitia ipsa ducimus cumque!",
-    "techs": "JavaScript, Node.JS"
-  },
-  {
-    "img": display,
-    "title": "Projetinho Dev",
-    "links": {
-      "deploy": "https://web.whatsapp.com", 
-      "frontend": "https://app.rocketseat.com.br/dashboard", 
-      "backend": "https://github.com/sirlucaslima", 
-      "behance": "https://www.behance.net/"
-    },
-    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis consectetur labore, distinctio unde pariatur ratione! Totam distinctio corporis ipsam beatae quam rerum obcaecati quidem provident commodi quibusdam, maiores nostrum magnam ratione velit. Repudiandae doloremque fugit ut fuga distinctio, facilis voluptates earum deserunt ad ullam, vero esse necessitatibus velit et cum assumenda quasi id quidem? Ea obcaecati dolores cum aliquam, saepe nostrum quo! Iusto placeat nam reprehenderit rem consectetur. In repellat a atque quos explicabo iure incidunt quibusdam et possimus fuga. Ex vel assumenda ratione, ullam ea doloremque maxime! Animi neque adipisci aliquid, veniam rem exercitationem quisquam quas? Maxime laudantium obcaecati ex quidem nemo. Iusto incidunt delectus necessitatibus qui possimus explicabo, reiciendis voluptatibus magnam, facilis itaque corrupti debitis, tempore distinctio dolorem soluta iure aspernatur culpa nihil? Maxime fugit deleniti quis quasi. Cum, nulla corporis quam tempora impedit alias fuga optio possimus voluptates, deleniti eum assumenda sint est mollitia ipsa ducimus cumque!",
-    "techs": "JavaScript, Node.JS"
-  },
-  {
-    "img": display,
+    "img1": airBnbClone1,
+    "img2": airBnbClone2,
     "title": "Projetinho Dev",
     "links": {
       "deploy": "https://web.whatsapp.com", 
