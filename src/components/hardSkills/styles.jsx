76cb5bf24b57ab38e27ee79b9afdc008e17536ec
JoviTech <jovitechgaropaba@gmail.com>
2023-04-16 04:53:09 -0300

    hardskills

--- src/components/hardSkills/styles.jsx
+++ src/components/hardSkills/styles.jsx
@@ -3,13 +3,14 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 65.3rem;
   height: 80.8rem;
-  padding: 0 2.4rem;
+  padding: 0 2.3rem;
 
   border-radius: 24px;
 
   display: flex;
   flex-wrap: wrap;
   align-content: flex-start;
+  justify-content: center;
   gap: 2.4rem;
 
   overflow-y: scroll;
