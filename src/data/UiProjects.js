d4222b050270b2f8d591589afda59374be46b747
JoviTech <developerlucaslima@gmail.com>
2023-04-16 22:54:17 -0300

    Description vovo carolina e massoterapia

--- src/data/UiProjects.js
+++ src/data/UiProjects.js
@@ -6,6 +6,8 @@ import map1 from "../assets/projectsUI/map1.png"
 import map2 from "../assets/projectsUI/map2.png"
 import vovoCarolina1 from "../assets/projectsUI/vovoCarolina1.png"
 import vovoCarolina2 from "../assets/projectsUI/vovoCarolina2.png"
+import portofolio1 from "../assets/projectsDev/portofolio1.png"
+import portofolio2 from "../assets/projectsDev/portofolio2.png"
 
 export const UiProjects = [
   {
@@ -15,17 +17,17 @@ export const UiProjects = [
     "links": {
       "behance": "https://www.behance.net/gallery/168299351/LOGO-POUSADA-CASA-ROSA"
     },
-    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis consectetur labore, distinctio unde pariatur ratione! Totam distinctio corporis ipsam beatae quam rerum obcaecati quidem provident commodi quibusdam, maiores nostrum magnam ratione velit. Repudiandae doloremque fugit ut fuga distinctio, facilis voluptates earum deserunt ad ullam, vero esse necessitatibus velit et cum assumenda quasi id quidem? Ea obcaecati dolores cum aliquam, saepe nostrum quo! Iusto placeat nam reprehenderit rem consectetur. In repellat a atque quos explicabo iure incidunt quibusdam et possimus fuga. Ex vel assumenda ratione, ullam ea doloremque maxime! Animi neque adipisci aliquid, veniam rem exercitationem quisquam quas? Maxime laudantium obcaecati ex quidem nemo. Iusto incidunt delectus necessitatibus qui possimus explicabo, reiciendis voluptatibus magnam, facilis itaque corrupti debitis, tempore distinctio dolorem soluta iure aspernatur culpa nihil? Maxime fugit deleniti quis quasi. Cum, nulla corporis quam tempora impedit alias fuga optio possimus voluptates, deleniti eum assumenda sint est mollitia ipsa ducimus cumque!",
-    "techs": "Adobe illustrator"
+    "description": "Desenvolvi este logotipo para uma pousada situada na cidade de Canela-RS, onde o cliente solicitou que a fachada do local fosse representada. Para transmitir um ambiente acolhedor e não tão formal, optei por criar o desenho a mão, utilizando a mesa digitalizadora. Além disso, a paleta de cores foi inspirada em uma fotografia da própria fachada.",
+    "techs": "Adobe illustrator, Mesa Digitalizadora"
   },
   {
     "img1": massoterapia1,
     "img2": massoterapia2,
-    "title": "Logo - Massoteropaia Márcia Aurelia",
+    "title": "Identidade Visual - Massoteropaia Márcia Aurelia",
     "links": {
       "behance": "https://www.behance.net/gallery/105279793/IDENTIDADE-VISUAL-E-LOGO-MARCIA-AURELIA-MASSOTERAPIA"
     },
-    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis consectetur labore, distinctio unde pariatur ratione! Totam distinctio corporis ipsam beatae quam rerum obcaecati quidem provident commodi quibusdam, maiores nostrum magnam ratione velit. Repudiandae doloremque fugit ut fuga distinctio, facilis voluptates earum deserunt ad ullam, vero esse necessitatibus velit et cum assumenda quasi id quidem? Ea obcaecati dolores cum aliquam, saepe nostrum quo! Iusto placeat nam reprehenderit rem consectetur. In repellat a atque quos explicabo iure incidunt quibusdam et possimus fuga. Ex vel assumenda ratione, ullam ea doloremque maxime! Animi neque adipisci aliquid, veniam rem exercitationem quisquam quas? Maxime laudantium obcaecati ex quidem nemo. Iusto incidunt delectus necessitatibus qui possimus explicabo, reiciendis voluptatibus magnam, facilis itaque corrupti debitis, tempore distinctio dolorem soluta iure aspernatur culpa nihil? Maxime fugit deleniti quis quasi. Cum, nulla corporis quam tempora impedit alias fuga optio possimus voluptates, deleniti eum assumenda sint est mollitia ipsa ducimus cumque!",
+    "description": "Essa marca foi inspirada no Deus Ganesha, que é representado por um elefante. Utilizei as duas iniciais da cliente (M e A) de forma criativa, de modo que elas se encaixassem nas formas do elefante. As cores escolhidas foram baseadas na cultura hindu, para garantir a coerência com o tema da marca.",
     "techs": "Adobe illustrator, Adobe Photoshop"
   },
   {
@@ -35,8 +37,8 @@ export const UiProjects = [
     "links": {
       "behance": "https://www.behance.net/gallery/105279793/IDENTIDADE-VISUAL-E-LOGO-MARCIA-AURELIA-MASSOTERAPIA"
     },
-    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis consectetur labore, distinctio unde pariatur ratione! Totam distinctio corporis ipsam beatae quam rerum obcaecati quidem provident commodi quibusdam, maiores nostrum magnam ratione velit. Repudiandae doloremque fugit ut fuga distinctio, facilis voluptates earum deserunt ad ullam, vero esse necessitatibus velit et cum assumenda quasi id quidem? Ea obcaecati dolores cum aliquam, saepe nostrum quo! Iusto placeat nam reprehenderit rem consectetur. In repellat a atque quos explicabo iure incidunt quibusdam et possimus fuga. Ex vel assumenda ratione, ullam ea doloremque maxime! Animi neque adipisci aliquid, veniam rem exercitationem quisquam quas? Maxime laudantium obcaecati ex quidem nemo. Iusto incidunt delectus necessitatibus qui possimus explicabo, reiciendis voluptatibus magnam, facilis itaque corrupti debitis, tempore distinctio dolorem soluta iure aspernatur culpa nihil? Maxime fugit deleniti quis quasi. Cum, nulla corporis quam tempora impedit alias fuga optio possimus voluptates, deleniti eum assumenda sint est mollitia ipsa ducimus cumque!",
-    "techs": "Adobe Photoshop"
+    "description": "Desenvolvi este mapa manualmente, utilizando uma mesa digitalizadora. Foi meu primeiro trabalho utilizando essa ferramenta, e tive o cuidado de dar atenção aos detalhes e à qualidade do desenho. O mapa foi encomendado por uma pousada localizada em Gramado-RS, com o objetivo de orientar os turistas a apreciarem as belas paisagens e natureza da região. Para criar um mapa completo e preciso, pesquisei minuciosamente as rotas disponíveis e os principais pontos turísticos da cidade. A partir disso, tracei as rotas mais adequadas e destaquei os principais atrativos, utilizando cores vibrantes para chamar a atenção e tornar a leitura mais fácil. Com o mapa em mãos, os turistas conseguem se locomover com mais facilidade e segurança, descobrindo novos lugares e se encantando com as belezas naturais da região. Foi gratificante desenvolver um projeto que contribui para a experiência positiva dos visitantes e a divulgação do turismo local.",
+    "techs": "Adobe Photoshop, Mesa Digitalizadora"
   },
   {
     "img1": vovoCarolina1,
