2eaaa6dc440c806976fdaf646ea84378c6dbd862
JoviTech <developerlucaslima@gmail.com>
2023-05-16 03:25:20 -0300

    fixed up cv and about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,5 @@
 import { Container } from "./styles"
-import Gramado from "../../assets/map.png"
+import CV from "../../assets/fullstack.pdf"
 
 export function About(){
   return(
@@ -8,20 +8,24 @@ export function About(){
         <h1>Sobre</h1>
         <div className="resume">
           <span>CV</span>
-          <a href={Gramado} download>
+          <a href={CV} download>
             <i className="ri-file-download-line" />
           </a>
         </div>
       </div>
       <div className="textarea">
         <p>
-          Como um desenvolvedor Full-Stack de JavaScript com experiência como designer, trago uma perspectiva e conjunto de habilidades únicos para o meu trabalho. 
-        </p>
-        <p>
-          Sou proficiente no uso de ferramentas de design como Illustrator, Photoshop e Figma para criar wireframes e protótipos que ajudam a dar vida às ideias. Embora meu trabalho seja dedicado ao desenvolvimento, essa experiência me permite trabalhar em estreita colaboração com designers para garantir que sua visão seja implementada de forma eficaz.
-        </p>
-        <p>
-          Minhas habilidades de desenvolvimento incluem a construção de aplicações web usando tecnologias como React, Next.js, Tailwind e Styled Components, e trabalhar com tecnologias do lado do servidor e bancos de dados, como Node.js, Express, Nest.js, Knex.js, SQLite e MongoDB (Restful API).
+          Como desenvolvedor Full-Stack JavaScript com experiência em design, trago uma
+          perspectiva única e um conjunto de habilidades para o meu trabalho. Sou proficiente
+          no uso de ferramentas de design, como Illustrator, Photoshop e Figma, para criar
+          wireframes e protótipos que dão vida às ideias. Além disso, minhas habilidades
+          de desenvolvimento incluem HTML e CSS, JavaScript, TypeScript, React, Next.js,
+          Stitches.css, Tailwind.css, Styled Components, React Hooks, ContextAPI, Zustand,
+          entre outras. Também estou familiarizado com as necessidades de servidor e banco
+          de dados, trabalhando com Node.js, Nest.js, Express, Fastify, Knex.js, Prisma, Jest,
+          Vitest, SQLite e Postgres. Combinando essas habilidades, sou capaz de trabalhar em
+          estreita colaboração com equipes de design para garantir a implementação eficaz
+          da visão do projeto
         </p>
       </div>
     </Container>
