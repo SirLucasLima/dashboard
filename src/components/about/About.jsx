b9e58950dc9cf1edb5b9a84a08778d2ab0a55317
Lucas Lima <101366755+developerlucaslima@users.noreply.github.com>
2023-12-20 15:13:32 -0300

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,7 +15,13 @@ export function About(){
       </div>
       <div className="textarea">
         <p>
-          I am a Full-Stack developer with design skills. I have experience using tools like Illustrator, Photoshop, and Figma for creating wireframes and prototypes. My development expertise spans web and mobile areas, with proficiency in Java (SpringBoot and Java Android) and JavaScript (Node.js/Nest.js and React/Next.js). The integration of these skills enables me to closely collaborate with design teams, ensuring the effective implementation of the project's vision. Additionally, I have knowledge in Docker, DDD, OOP, TDD, and SOLID. I am constantly updating my skill set to provide the best available solutions.
+          I am a Full-Stack developer with design skills. I have experience using tools like Illustrator, 
+          Photoshop, and Figma for creating wireframes and prototypes. My development expertise spans web 
+          and mobile areas, with proficiency in Java (SpringBoot and Java Android) and JavaScript 
+          (Node.js/Nest.js and React/Next.js). The integration of these skills enables me to closely 
+          collaborate with design teams, ensuring the effective implementation of the project's vision. 
+          Additionally, I have knowledge in Docker, DDD, OOP, TDD, and SOLID. I am constantly updating my 
+          skill set to provide the best available solutions.
         </p>
       </div>
     </Container>
