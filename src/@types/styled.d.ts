ca2311360b20e6b2776f966a5161f4675ea1fe78
developerlucaslima <developerlucaslima@gmail.com>
2025-01-15 12:31:27 -0300

    feat: theme typography

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -13,10 +13,11 @@ declare module 'styled-components' {
       mutedForeground: string
       destructive: string
       destructiveForeground: string
+      shadow: string
+      shadowForeground: string
       border: string
       ring: string
       transparent: string
-      // colorful
       green: string
       blue: string
       cyan: string
@@ -25,6 +26,32 @@ declare module 'styled-components' {
       rose: string
       purple: string
     }
-    radius: string
+    border: {
+      radius: string
+      circle: string
+    }
+    typography: {
+      fontSize: {
+        sm: string
+        md: string
+        lg: string
+        xl: string
+      }
+      fontWeight: {
+        normal: number
+        medium: number
+        bold: number
+      }
+      lineHeight: {
+        sm: number
+        md: number
+        lg: number
+        xl: number
+      }
+    }
+    round: {
+      radius: string
+      circle: string
+    }
   }
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,23 +5,32 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: "Fira Mono", serif;
+    color: ${({ theme }) => theme.colors.foreground};
   }
 
   :focus {
     outline: none;
-    /* box-shadow: 0 0 0 .5px ${({ theme }) => theme.colors.ring}; */
   }
 
   body {
     background: ${({ theme }) => theme.colors.background};
-    color: ${({ theme }) => theme.colors.foreground};
     -webkit-font-smoothing: antialiased;
   }
 
+  ul {
+    list-style: none;
+  }
+
+  a {
+    cursor: pointer;
+    text-decoration: none;
+  }
+
   body, input, textarea, button {
-    font-family: 'Roboto Slab', serif;
     font-weight: 400;
-    font-size: 1rem;
+    font-size: 18px;
+    /* line-height: 3.6px; */
     outline: none;
   }
 `
