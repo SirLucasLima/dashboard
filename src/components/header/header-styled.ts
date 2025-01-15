68725a4c0731b48b403840cf3a07a0c36463d346
developerlucaslima <developerlucaslima@gmail.com>
2025-01-15 12:36:12 -0300

    feat: header component

---
+++ src/components/header/header-styled.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+export const HeaderStyled = styled.a`
+  z-index: 10;
+  position: fixed;
+  height: 2.5rem;
+  width: 100vw;
+
+  display: flex;
+  border-bottom: 0.5px solid ${({ theme }) => theme.colors.border};
+`
+
+export const TitleContainerStyled = styled.div`
+  width: 20rem;
+
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+
+  border-right: 0.5px solid ${({ theme }) => theme.colors.border};
+`
+
+export const TabsListContainerStyled = styled.ul`
+  width: 100%;
+  overflow: hidden;
+
+  display: flex;
+
+  border-right: 0.5px solid ${({ theme }) => theme.colors.border};
+
+  > li {
+    width: auto;
+    overflow: hidden;
+    padding: 1rem;
+
+    display: flex;
+    align-items: center;
+
+    border-right: 0.5px solid ${({ theme }) => theme.colors.border};
+  }
+`
+
+export const ContactContainerStyled = styled.a`
+  width: 15rem;
+
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
