f87e8166c27dcf0532a2b2fc5df07395d8c63506
developerlucaslima <developerlucaslima@gmail.com>
2025-01-15 12:34:09 -0300

    feat: card profile pic

--- src/components/card-profile-pic/_index.tsx
+++ src/components/card-profile-pic/_index.tsx
@@ -1,12 +1,12 @@
 import profilePic from '@/assets/profile-pic.webp'
 
 import { Card } from '../card/_index'
-import { ProfilePicStyled } from './card-profile-pic-styled'
+import { CardProfilePicStyled } from './card-profile-pic-styled'
 
 export const CardProfilePic = () => {
   return (
     <Card variant="secondary" overflow="hidden" padding={false}>
-      <ProfilePicStyled src={profilePic} alt="imagem de perfil" />
+      <CardProfilePicStyled src={profilePic} alt="imagem de perfil" />
     </Card>
   )
 }
--- src/components/card-profile-pic/card-profile-pic-styled.ts
+++ src/components/card-profile-pic/card-profile-pic-styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const ProfilePicStyled = styled.img`
+export const CardProfilePicStyled = styled.img`
   display: block;
   width: 100%;
   height: 100%;
