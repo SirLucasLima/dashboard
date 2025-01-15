60ffe242c100b92d6b12f399239812c4954dd756
developerlucaslima <developerlucaslima@gmail.com>
2025-01-15 12:34:24 -0300

    feat: card social links

---
+++ src/components/card-social-links/card-social-links-styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const CardSocialLinksStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 100%;
+`
