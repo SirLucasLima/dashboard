1a35a8cac31257d5d856696cf63a9bb8a874410b
developerlucaslima <developerlucaslima@gmail.com>
2025-01-16 11:50:28 -0300

    feat: routes and pages development

--- src/pages/home/home-types.ts
+++
@@ -1,4 +0,0 @@
-export interface GridStylesProps {
-  $colSpan?: number
-  $rowSpan?: number
-}
--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,15 +1,23 @@
 import { RouteObject } from 'react-router-dom'
 
 import { AppLayout } from '@/layouts/app-layout'
+import { HomeLayout } from '@/layouts/home-layout'
+import { AboutMe } from '@/pages/about-me/about-me'
+import { ContactMe } from '@/pages/contact-me/contact-me'
 import { Home } from '@/pages/home/home'
 
 export const appRoutes: RouteObject[] = [
   {
     path: '/',
+    element: <HomeLayout />,
+    children: [{ index: true, element: <Home /> }],
+  },
+  {
+    path: '/',
     element: <AppLayout />,
     children: [
-      { index: true, element: <Home /> },
-      { path: '/teste', element: <Home /> },
+      { path: '/about-me', element: <AboutMe /> },
+      { path: '/contact-me', element: <ContactMe /> },
     ],
   },
 ]
