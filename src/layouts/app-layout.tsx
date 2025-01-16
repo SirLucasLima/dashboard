0cb3cbedde43ce1682a8266f85a0a42ae0ddf5ca
developerlucaslima <developerlucaslima@gmail.com>
2025-01-16 11:34:49 -0300

    feat: app layout

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -1,27 +1,16 @@
 import { Outlet } from 'react-router-dom'
-import styled from 'styled-components'
 
 import { AppContainers } from '@/components/app-containers/_index'
 import { Bottom } from '@/components/bottom/_index'
 import { Header } from '@/components/header/_index'
 
-const AppLayoutStyled = styled.div`
-  width: 100wvh;
-  height: 100vh;
-  overflow: hidden;
-  padding-top: 2.5rem;
-  padding-bottom: 2.5rem;
-`
-
 export function AppLayout() {
   return (
     <>
       <Header />
-      <AppLayoutStyled>
-        <AppContainers>
-          <Outlet />
-        </AppContainers>
-      </AppLayoutStyled>
+      <AppContainers>
+        <Outlet />
+      </AppContainers>
       <Bottom />
     </>
   )
