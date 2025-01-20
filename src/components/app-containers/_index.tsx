8d814a2649cff974faed5e0f4c47d394a822bf94
developerlucaslima <developerlucaslima@gmail.com>
2025-01-20 19:40:58 -0300

    feat: app containers layout

--- src/components/app-containers/_index.tsx
+++ src/components/app-containers/_index.tsx
@@ -1,18 +1,20 @@
-import { SideBar } from '../side-bar/_index'
-import {
-  AppContainersStyled,
-  ContentContainerStyled,
-  SideBarContainerStyled,
-} from './app-container-styled'
+import { Explorer } from '../explorer/_index'
+import { Footer } from '../footer/_index'
+import { Header } from '../header/_index'
+import { SideMenu } from '../side-menu/_index'
+import { AppLayoutStyled, EditorLayoutStyled } from './app-container-styled'
 import type { AppContainersProps } from './app-container-types'
 
 export const AppContainers = ({ children }: AppContainersProps) => {
   return (
-    <AppContainersStyled>
-      <SideBarContainerStyled>
-        <SideBar />
-      </SideBarContainerStyled>
-      <ContentContainerStyled>{children}</ContentContainerStyled>
-    </AppContainersStyled>
+    <AppLayoutStyled>
+      <Header />
+      <EditorLayoutStyled>
+        <SideMenu />
+        <Explorer />
+        <div>{children}</div>
+      </EditorLayoutStyled>
+      <Footer />
+    </AppLayoutStyled>
   )
 }
--- src/components/app-containers/app-container-styled.ts
+++ src/components/app-containers/app-container-styled.ts
@@ -1,32 +1,14 @@
 import styled from 'styled-components'
 
-export const AppContainersStyled = styled.div`
-  width: 100wvh;
-  height: 100vh;
-  padding-top: 2.5rem;
-  padding-bottom: 2.5rem;
-  overflow: hidden;
-
-  display: flex;
-  border-bottom: 0.5px solid ${({ theme }) => theme.colors.border};
+export const AppLayoutStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-rows: 2rem 1fr 1.5rem;
 `
 
-export const SideBarContainerStyled = styled.div`
-  width: 15rem;
-
-  display: flex;
-  /* visibility: hidden;
-  position: fixed; */
-
-  border-right: 0.5px solid ${({ theme }) => theme.colors.border};
-`
-
-export const ContentContainerStyled = styled.ul`
-  flex: 1;
-
-  overflow: hidden;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+export const EditorLayoutStyled = styled.div`
+  display: grid;
+  grid-template-columns: 3.5rem 16rem 1fr;
+  max-height: 100%;
 `
