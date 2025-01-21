b1d818aefcf34dd16f87fe711ac9255c838b8776
developerlucaslima <developerlucaslima@gmail.com>
2025-01-20 22:12:49 -0300

    feat: simplify Home component by removing unused Presentation and BackgroundGradient imports

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,7 @@
-import { BackgroundGradient } from '@/components/bg-gradient/_index'
-import { Presentation } from '@/components/presentation/_index'
-
 export const Home = () => {
   return (
     <>
-      <Presentation />
-      <BackgroundGradient />
+      <h1>home</h1>
     </>
   )
 }
