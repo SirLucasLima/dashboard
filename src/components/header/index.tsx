3ad10a278b2c6c8311110f9bafed1b66fc525794
developerlucaslima <developerlucaslima@gmail.com>
2025-01-22 12:09:40 -0300

    fix: improve header display by conditionally rendering separator for current page name

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,13 +7,15 @@ import { StyledCloseButton, StyledHeader } from './styled-header'
 
 export const Header = () => {
   const currentPageName = useExplorerStore(state => state.currentPageName)
-  console.log('currentPageName: ', currentPageName)
 
   return (
     <StyledHeader>
       <div className="empty-space">&nbsp;</div>
-
-      <span>{currentPageName} - dev-lucas-lima - Dev Studio Code</span>
+      <span>
+        <span>{currentPageName}</span>
+        <span>{currentPageName ? ' - ' : ''}</span>
+        <span>dev-lucas-lima - Dev Studio Code</span>
+      </span>
       <StyledCloseButton as={Link} to={'/'} className="close">
         <X strokeWidth={2} size={14} />
       </StyledCloseButton>
