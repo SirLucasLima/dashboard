2ca9dda8a05fdc1c6794ac3c5002b908adbe34cd
developerlucaslima <developerlucaslima@gmail.com>
2025-01-24 12:13:39 -0300

    style: update About Me section for improved clarity and formatting

--- src/pages/about-me/content.ts
+++ src/pages/about-me/content.ts
@@ -1,12 +1,7 @@
 /* eslint-disable max-len */
-export const content = `[About Me]
-Desenvolvedor full stack com mais de 2 anos de experiência em desenvolvimento de software, 3 anos de experiência em design e 7 anos de experiência no setor financeiro. Gosto de praticar esportes, levar um estilo de vida pacífico e saudável. Apaixonado por crescimento contínuo, busco ser uma pessoa melhor a cada dia e me cercar de pessoas boas e talentosas que também buscam tornar o mundo ao seu redor um lugar melhor.
+export const content = `# About Me
  
-[Education]
-#Análise e Desenvolvimento de Sistemas:# Faculdade Descomplica | Florianópolis - SC, BR | 06/2022 - Presente
-#Economia (descontinuado):# Unisinos | São Leopoldo - RS, BR | 01/2018 - 12/2020
-#Arquitetura e Urbanismo (descontinuado):# Feevale | Novo Hamburgo - RS, BR | 02/2013 - 06/2017
+- 💼 Full Stack Developer with over 2 years of experience in software development, 3 years in design, and 7 years in the financial sector.
  
-[Courses]
-#Full-Stack Rocketseat:# JavaScript, TypeScript, Node, React, React Native, Java, SpringBoot | 06/2022 - Presente
-#English C1:# Erin School | Dublin - Ireland | 01/2019 - 07/2019`
+- 🌱 Passionate about continuous growth, I strive to be a better person each day and surround myself with good and talented people who also seek to make the world around them a better place.
+ `
