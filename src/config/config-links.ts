446e8983064d3a32f68431c12c2941a0cc588762
developerlucaslima <developerlucaslima@gmail.com>
2025-01-24 10:29:07 -0300

    feat: add configuration for social links and custom theme settings

---
+++ src/config/config-links.ts
@@ -0,0 +1,17 @@
+export interface ConfigLinks {
+  gitHub: string
+  projectGitHub: string
+  linkedIn: string
+  instagram: string
+  whatsApp: string
+  behance: string
+}
+
+export const configLinks: ConfigLinks = {
+  gitHub: 'https://github.com/developerlucaslima',
+  projectGitHub: 'https://github.com/developerlucaslima/portfolio-client',
+  linkedIn: 'https://www.linkedin.com/in/developerlucaslima/',
+  instagram: 'https://www.instagram.com/devlucaslima/',
+  whatsApp: 'https://wa.me/555499665259',
+  behance: 'https://www.behance.net/developerlucaslima',
+}
