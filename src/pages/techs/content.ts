5bfd9de8e8d9c88897464df9278e98ccdf5c7fee
developerlucaslima <developerlucaslima@gmail.com>
2025-01-28 12:35:26 -0300

    feat: add Techs page with technologies content and code preview component

---
+++ src/pages/techs/content.ts
@@ -0,0 +1,66 @@
+export const content = `# Technologies
+ 
+🎨 [Frontend]
+- JavaScript / TypeScript
+- React.js
+- Next.js
+- Tailwind CSS
+- Material UI
+- Zustand
+- React Query
+- Styled-components
+- ShadCN UI
+- Radix UI
+- Vue.js
+- Vuetify
+- Vue Query
+- Pinia
+ 
+🛠️ [Backend]
+- JavaScript / TypeScript
+- Node.js
+- Fastify.js
+- Express.js
+- Java
+- Spring Boot
+ 
+📱 [Mobile]
+- React Native
+- Android
+- Jetpack Android
+ 
+🗄️ [Databases]
+- PostgreSQL
+- MySQL
+- Oracle Database
+ 
+🔧 [Tools]
+- Git
+- GitHub
+- GitLab
+- Docker
+- SVN
+- Bitbucket
+- Azure
+- Eclipse
+- Android Studio
+ 
+🧪 [Testing]
+- Vitest
+- SuperTest
+- React Testing Library
+ 
+🎨 [Design]
+- Adobe Illustrator
+- Adobe Photoshop
+- CorelDRAW
+- Web Development
+- Figma
+- Web Design`
+
+// 💻 [Programming Languages]
+// - TypeScript
+// - JavaScript
+// - Java
+// - SQL
+// - XML
