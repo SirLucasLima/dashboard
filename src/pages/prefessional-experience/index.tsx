6f8fd0717037ecd006e2f562e3dfb33a06d04fea
developerlucaslima <developerlucaslima@gmail.com>
2025-01-28 12:35:49 -0300

    feat: add Professional Experience page with detailed work history and skills

---
+++ src/pages/prefessional-experience/index.tsx
@@ -0,0 +1,7 @@
+import { CodePreview } from '@/components/code-preview'
+
+import { content } from './content'
+
+export const ProfessionalExperience = () => {
+  return <CodePreview content={content} />
+}
